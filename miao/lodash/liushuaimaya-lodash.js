c9b97bc38d3704076db2c0203bb627b37d3a4679
liushuai <313628216@qq.com>
2019-07-10 17:40:48 +0800

    chunk

--- miao/lodash/liushuaimaya-lodash.js
+++ miao/lodash/liushuaimaya-lodash.js
@@ -2,4 +2,8 @@ var liushuaimaya = {
   compact: function (ary) {
     return ary.filter(it => it)
   },
+  chunk: function(ary, size = 1) {
+    let res = [];
+    ary.forEach(a, i => res[0])
+  }
 }
