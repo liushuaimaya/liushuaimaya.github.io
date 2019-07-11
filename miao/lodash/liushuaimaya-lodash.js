021f53b7723f7ce2e10b86828f948666aa1be240
liushuai <313628216@qq.com>
2019-07-11 13:07:06 +0800

    filter

--- miao/lodash/liushuaimaya-lodash.js
+++ miao/lodash/liushuaimaya-lodash.js
@@ -2,8 +2,21 @@ var liushuaimaya = {
   compact: function (ary) {
     return ary.filter(it => it)
   },
-  chunk: function(ary, size = 1) {
+  chunk: function (ary, size = 1) {
     let res = [];
-    ary.forEach(a, i => res[0])
+    for (let i = 0; i < ary.length / size | 0; i++) {
+      res.push([]);
+    };
+    ary.forEach((a, i) => { res[i / size | 0].push(a) });
+    return res;
+  },
+  filter: function filter(collection, predicate) {
+    let res = [];
+      for (let index in collection) {
+        if (predicate(collection[index], index, collection)) {
+          res.push(collection[index]);
+        }
+    }
+    return res;
   }
 }
