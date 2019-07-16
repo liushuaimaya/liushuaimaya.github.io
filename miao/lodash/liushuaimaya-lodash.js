13198675b0981a9f9250bbdc2a63facddba2cce5
liushuai <313628216@qq.com>
2019-07-16 20:47:26 +0800

    every...

--- miao/lodash/liushuaimaya-lodash.js
+++ miao/lodash/liushuaimaya-lodash.js
@@ -10,6 +10,7 @@ var liushuaimaya = {
     ary.forEach((a, i) => { res[i / size | 0].push(a) });
     return res;
   },
+  flip: f => (...args) => f(...args.reverse()),
   filter: (collection, predicate) => {
     let res = [];
     for (let index in collection) {
@@ -25,5 +26,36 @@ var liushuaimaya = {
   some: (ary, predicate) => {
     return ary.reduce((res, a, i, ary) => res || predicate(a, i, ary), false);
   },
-  flip: f => (...args) => f(...args.reverse())
+  memoize: f => {
+    let memo = {};
+    return function (...args) {
+      if (args in memo) {
+        return memo[args];
+      } else {
+        return memo[args] = f(...args);
+      }
+    }
+  },
+  spread: f => (args) => f(...args),
+  any: (f, n = f.length) => (...args) => f(...args.slice(0, n)),
+  mapvalues: (obj, f) => {
+    for (key in obj) {
+      obj[key] = f(obj[key], key, obj);
+    }
+    return obj;
+  },
+  bind: (f, ...args1) => (...args2) => f(...args1, ...args2),
+  flatten: ary => [].cancat(...ary),
+  flattenDeep:  ary => {
+    while (ary.some(Array.isArray)) {
+      ary = [].concat(...ary);
+    }
+    return ary;
+  },
+  flattenDepth:  (ary, depth = 1) => {
+    while (depth-- > 0) {
+      ary = [].concat(...ary);
+    }
+    return ary;
+  }
 }
