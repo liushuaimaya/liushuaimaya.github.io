e2d46a2e99bd7075e93d1adeb53e999b90c2fc95
liushuai <313628216@qq.com>
2019-08-05 18:24:05 +0800

    diff

--- miao/lodash/liushuaimaya-lodash.js
+++ miao/lodash/liushuaimaya-lodash.js
@@ -67,10 +67,11 @@ var liushuaimaya = {
       }
     }
   },
-  difference: function(ary, ary2) {
-    const set2 = new Set(ary2);
-    return ary.filter(x => !set2.has(x));
+  difference: function(ary, ...args) {
+    let set = new Set();
+    args.forEach(arg => arg.forEach(val => set.add(val)));
+    return ary.filter(x => !set.has(x));
   }
 }
 
-console.log(liushuaimaya.difference([1,2, 1],[2,3]))
+// console.log(liushuaimaya.difference([1,2,3,4,5,6,7,8],[1,3],[4,8],[6]))
