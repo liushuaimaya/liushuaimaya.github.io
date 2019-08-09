1dbafb23dc65149f91cc4f8ae7af87cad3a8f0ad
liushuai <313628216@qq.com>
2019-08-09 17:37:17 +0800

    identity

--- miao/lodash/liushuaimaya-lodash.js
+++ miao/lodash/liushuaimaya-lodash.js
@@ -3,6 +3,9 @@ var liushuaimaya = {
   compact: ary => ary.filter(Boolean),
   difference: (ary, ...args) => ary.filter(x => !(args.reduce((res, a) => res.concat(a), [])).includes(x)),
   differenceBy: (ary, ...args) => typeof (args[args.length - 1]) == "function" ? ary.filter(x => !(args.slice(0, -1).reduce((res, a) => res.concat(a), [])).map(it => args[args.length - 1](it)).includes(args[args.length - 1](x))) : difference(ary, ...args),
+  drop: (arr, n = 1) => arr.silce(n),
+  dropRight: (arr, n = 1) => arr.silce(0, -n),
+  dropRightWhile: (arr, n = 1) => arr.silce(0, -n),
   property: path => obj => (Array.isArray(path) ? path : path.split(".")).reduce((res, it) => res[it], obj),
   flatten: ary => [].concat(...ary),
   flattenDeep: ary => ary.reduce((res, it) => res.concat(Array.isArray(it) ? flattenDeep(it) : it), []),
@@ -19,33 +22,25 @@ var liushuaimaya = {
   },
   every: (ary, predicate) => ary.reduce((res, a, i, ary) => res && predicate(a, i, ary), true),
   some: (ary, predicate) => ary.reduce((res, a, i, ary) => res || predicate(a, i, ary), false),
-  memoize: f => {
-    let memo = {};
-    return function (...args) {
-      if (args in memo) {
-        return memo[args];
-      } else {
-        return memo[args] = f(...args);
-      }
-    }
-  },
+  memoize: f => (memo = {}, (...args) => args in memo ? memo[args] : memo[args] = f(...args)),
   spread: f => args => f(...args),
   any: (f, n = f.length) => (...args) => f(...args.slice(0, n)),
-  mapvalues: (obj, f) => {
-    for (key in obj) {
-      obj[key] = f(obj[key], key, obj);
-    }
-    return obj;
-  },
+  // mapvalues: (obj, f) => {
+  //   for (key in obj) {
+  //     obj[key] = f(obj[key], key, obj);
+  //   }
+  //   return obj;
+  // },
   bind: (f, ...args1) => (...args2) => f(...args1, ...args2),
-  negate: (f, ...args) => () => !f(...args),
+  negate: f => (...args) => !f(...args),
   forOwn: function (obj, f) {
     var hasOwn = Object.prototype.hasOwnProperty;
     for (let key in obj) {
       if (hasOwn.call(obj, key)) {
         f(obj[key], key, obj);
-      }
+      } else break;
     }
   },
   identity: (...args) => args[0],
+  // iteratee: arg => (...args) => { }
 }
