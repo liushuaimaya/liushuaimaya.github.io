d11dfbe2798aeb309ab11c46ffd98a9867b19a29
liushuai <313628216@qq.com>
2019-08-09 20:35:38 +0800

    nothing tosay

--- miao/lodash/liushuaimaya-lodash.js
+++ miao/lodash/liushuaimaya-lodash.js
@@ -1,14 +1,8 @@
 var liushuaimaya = {
+  /** ONE LINE VERSION **/
   chunk: (ary, size) => ary.map((_, i) => i % size ? null : ary.slice(i, i + size)).filter(Boolean),
   compact: ary => ary.filter(Boolean),
   difference: (ary, ...args) => ary.filter(x => !args.flat().includes(x)),
-  differenceBy: (ary, ...args) => {
-    let f = it => it, last = args.pop();
-    if (typeof last == "function") f = last;
-    if (Array.isArray(last)) ary.push(last);
-    if (typeof last == "string") f = obj => last.split(".").reduce((re, p) => re[p], obj);
-    return ary.filter(x => !args.flat().map(it => f(it)).includes(f(x)));
-  },
   drop: (arr, n = 1) => arr.slice(n),
   dropRight: (arr, n = 1) => arr.slice(0, n ? -n : arr.length),
   // dropRightWhile: (arr, n = 1) => arr.slice(0, n ? -n : arr.length),
@@ -16,13 +10,7 @@ var liushuaimaya = {
   flatten: ary => [].concat(...ary),
   flattenDeep: ary => ary.reduce((res, it) => res.concat(Array.isArray(it) ? flattenDeep(it) : it), []),
   flattenDepth: (ary, depth = 1) => depth ? [].concat(...flattenDepth(ary, depth - 1)) : ary,
-  // indexOf: Array.prototype.indexOf.call,
-  indexOf: (arr, val, fromIndex = 0) => {
-    if (fromIndex < 0) fromIndex = fromIndex + arr.length;
-    for (let i = fromIndex; i < arr.length; i++) {
-      if (arr[i] == val) return i;
-    }
-  },
+  indexOf: Array.prototype.indexOf.call,
   flip: f => (...args) => f(...args.reverse()),
   filter: (collection, predicate) => {
     let res = [];
@@ -57,4 +45,23 @@ var liushuaimaya = {
   },
   identity: (...args) => args[0],
   // iteratee: arg => (...args) => { }
+
+
+
+
+
+  //** ORIGIN VERSION **/
+  differenceBy: (ary, ...args) => {
+    let f = it => it, last = args.pop();
+    if (typeof last == "function") f = last;
+    if (Array.isArray(last)) ary.push(last);
+    if (typeof last == "string") f = obj => last.split(".").reduce((re, p) => re[p], obj);
+    return ary.filter(x => args.flat().map(it => f(it)).includes(f(x)));
+  }
+  // indexOf: (arr, val, fromIndex = 0) => {
+  //   if (fromIndex < 0) fromIndex = fromIndex + arr.length;
+  //   for (let i = fromIndex; i < arr.length; i++) {
+  //     if (arr[i] == val) return i;
+  //   }
+  // },
 }
