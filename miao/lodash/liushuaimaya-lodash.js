33a84261c930bd8cd34c3c335f518f437fa91a5a
liushuai <313628216@qq.com>
2019-08-13 22:33:25 +0800

    nothing tosay

--- miao/lodash/liushuaimaya-lodash.js
+++ miao/lodash/liushuaimaya-lodash.js
@@ -223,7 +223,7 @@ function liushuaimayaSrc() {
     return array.filter((_, i) => transformed.indexOf(transformed[i]) == i);
   }
   const uniqWith = (array, comparator) => array.filter((arrVal, i) => !array.slice(0, i).some(othVal => comparator(arrVal, othVal)));
-  const unzip = () => "placeholder";
+  const unzip = array => zip(...array);
   const unzipWith = () => "placeholder";
   const without = () => "placeholder";
   const zip = (...arrays) => Array(Math.max(...arrays.map(it => it.length))).fill(0).map((_, i) => arrays.map(ary => ary[i]));
