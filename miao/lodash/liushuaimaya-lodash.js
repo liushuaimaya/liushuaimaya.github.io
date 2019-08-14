f2e93ae270bac9782fcf6529f2b6b166caf064a0
liushuai <313628216@qq.com>
2019-08-15 01:27:25 +0800

    nothing tosay

--- miao/lodash/liushuaimaya-lodash.js
+++ miao/lodash/liushuaimaya-lodash.js
@@ -265,6 +265,8 @@ function liushuaimayaSrc() {
     return args.flat().filter((arrVal, _, arr) => arr.reduce((cnt, othVal) => comparator(arrVal, othVal) ? cnt + 1 : cnt, 0) == 1);
   }
   const zip = (...arrays) => Array(Math.max(...arrays.map(it => it.length))).fill(0).map((_, i) => arrays.map(ary => ary[i]));
+  const zipObject = (props = [], values = []) => props.reduce((obj, prop, i) => (obj[prop] = values[i], obj), {});
+  // const zipObjectDeep = (props = [], values = []) => props.
   const flip = f => (...args) => f(...args.reverse());
   const forIn = (obj, func = identity) => {
     func = iteratee(func);
