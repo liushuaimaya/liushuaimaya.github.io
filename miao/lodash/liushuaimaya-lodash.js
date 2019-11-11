eeebff30fd732274e7ccb0fec11274db7ed61501
liushuai <313628216@qq.com>
2019-11-11 21:04:02 +0800

    little change

--- miao/lodash/liushuaimaya-lodash.js
+++ miao/lodash/liushuaimaya-lodash.js
@@ -421,7 +421,7 @@ function liushuaimayaSrc() {
   const set = (object, path, value) => {
     path = typeof path === "string" ? path.match(/\w+/g).map(it => isNaN(+it) ? it : +it) : path;
     path.reduce((res, p, i) => {
-      res[p] = res[p] || typeof path[i + 1] === "string" ? {} : typeof path[i + 1] === "number" ? [] : value;
+      res[p] = (res[p] || typeof path[i + 1] === "string" ? {} : typeof path[i + 1] === "number" ? [] : value);
       return res[p];
     }, object);
     return object;
